c8858fc1e7d58280e65f88024ef4b27b17de4427
Joshua Toguri <joshua.toguri@gmail.com>
2022-01-28 15:24:11 -0500

    implemented findKey function

---
+++ findKey.js
@@ -0,0 +1,36 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  }
+  console.log(`❌❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  return false;
+};
+
+const findKey = function(object, callback) {
+  for (const key of Object.keys(object)) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+// Test Cases
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 7 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), undefined);
