4a399ea05d07add3271523dfa6e4195310f2ec99
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-06 20:21:31 -0500

    implemented the head test using the mocha testing framework and chai assertion library

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,27 @@
-// test/headTest.js
+// Tests for head.js
 
-const head = require("../head");
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+const head = require('../head');
 
-// Test Code
+describe("#head", () => {
+  
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([81]), 81);
-assertEqual(head([]), undefined);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
