70f68005658036103971a797c65c3b3d67f2124b
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-10 11:51:45 -0500

    modified eqObjects to recursively check nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -1,60 +1,48 @@
-// Question for code review... can we use pointers to compare obejct equality?
+// Import the eqArrays function for use in eqObjects
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return true;
-  }
-  console.log(`❌❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  return false;
-};
+const { eqArrays } = require('./index');
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
-
-// Returns true if both objects have identical keys with identical values.
+// Function that takes in two objects and returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
+
 const eqObjects = function(object1, object2) {
+  
+  // Create arrays of the keys of both objects
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
+
+  // If the objects do not have the same number of keys we know they can not be equal
   if (keys1.length !== keys2.length) {
     return false;
   }
+
+  // Iterate over the keys from the first object
   for (const key of keys1) {
+    
+    // If the value at both keys is an array we compare them using the eqArrays function and only return if they are not equal
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
-    } else if (object1[key] !== object2[key]) {
+    }
+    
+    // If the values were not arrays we check to see if they are both objects, if so we recursively call eqObjects on them and only return if a recursive eqObjects call does not evaluate to true
+    else if (typeof object1[key] === 'object' && typeof object2[key] === 'object' && object1[key] !== null && object2[key] !== null) {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } 
+    
+    // If the values were not arrays or objects we can just directly compare them and only return if they are not equal
+    else if (object1[key] !== object2[key]) {
       return false;
     }
   }
+
+  // If we had no early returns we know they are equal and can return true
   return true;
 };
 
-// Test Cases
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+module.exports = {
+  eqObjects
+};
--- index.js
+++ index.js
@@ -2,36 +2,37 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const letterPositions = require('./letterPositions');
-const takeUntil = require('./takeUntil');
-const map = require('./map');
-const without = require('./without');
+// const findKey = require('./findKey');
+// const findKeyByValue = require('./findKeyByValue');
+// const flatten = require('./flatten');
+// const countLetters = require('./countLetters');
+// const countOnly = require('./countOnly');
+// const letterPositions = require('./letterPositions');
+// const takeUntil = require('./takeUntil');
+// const map = require('./map');
+// const without = require('./without');
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
+// const assertObjectsEqual = require('./assertObjectsEqual');
 const eqObjects = require('./eqObjects');
 
 module.exports = {
   head,
   tail,
   middle,
-  findKey,
-  findKeyByValue,
-  flatten,
-  countLetters,
-  countOnly,
-  letterPositions,
-  takeUntil,
-  map,
-  without,
+  // findKey,
+  // findKeyByValue,
+  // flatten,
+  // countLetters,
+  // countOnly,
+  // letterPositions,
+  // takeUntil,
+  // map,
+  // without,
   eqArrays,
   assertArraysEqual,
-  assertObjectsEqual,
-  eqObjects
+  // assertObjectsEqual,
+  eqObjects,
+  assertEqual
 };
